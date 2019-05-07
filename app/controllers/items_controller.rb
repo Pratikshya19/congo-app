2033508ea61fbb3d0eec6769928e223ffc32b630
kreestoff <christopher.l.mcintosh@outlook.com>
2019-05-07 09:55:57 -0500

    added search box and removed bootstrap

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
 
     def index
-        @items = Item.all 
-    end
+        @items = Item.all
+        @query = params[:q]
+        @categories = Category.all
+        if @query
+          @found_items = @items.select {|i| i.name.downcase.include? @query}
+        end
+      end
 
     def show
         @item = Item.find(params[:id])
